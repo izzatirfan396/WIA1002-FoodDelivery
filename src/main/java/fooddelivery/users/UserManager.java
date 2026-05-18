ccf792482bc032043fb4f7315905e316a8831ad3
izzatirfan396 <24001467@siswa.um.edu.my>
2026-05-18 11:45:21 +0800

    add User and Restaurant classes

--- src/main/java/fooddelivery/users/UserManager.java
+++ src/main/java/fooddelivery/users/UserManager.java
@@ -1,27 +1,17 @@
 package fooddelivery.users;
 
-/**
- * Module 1 - User & Restaurant Management
- * Member: [M1 Name]
- *
- * Data Structure: ArrayList / LinkedList
- * Responsibilities:
- *   - Register and manage customers
- *   - Register and manage restaurants
- *   - Display user and restaurant lists
- */
-public class UserManager {
 
-    // TODO (M1): declare your ArrayList or LinkedList for users and restaurants here
+public class UserManager<T> {
+    
+    LinkedList<T> list ;
 
     public UserManager() {
-        // TODO (M1): initialise your data structures here
+        list = new LinkedList<>();
     }
 
     public void showMenu() {
-        // TODO (M1): implement the sub-menu for this module
+       
     }
 
-    // TODO (M1): add your methods below (addUser, removeUser, displayUsers, etc.)
-
+  
 }
