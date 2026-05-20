71ef5b65562fe48af368f0d0365c41c83d456eaf
izzatirfan396 <24001467@siswa.um.edu.my>
2026-05-20 19:30:29 +0800

    update for show menu ( still  in progress 0

--- src/main/java/fooddelivery/users/UserManager.java
+++ src/main/java/fooddelivery/users/UserManager.java
@@ -1,7 +1,11 @@
 package fooddelivery.users;
 
+import fooddelivery.utils.InputHelper;
+import java.util.Scanner;
+
 
 public class UserManager {
+    
     
     MyLinkedList<User> userList ;
     MyLinkedList<Restaurant> restaurantList ;
@@ -43,7 +47,35 @@ public class UserManager {
     
    
     
-    public void showMenu() {
+    public void showMenu() { 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- User & Restaurant Management ---");
+        System.out.println();
+        System.out.println("1. Register New User ");
+        System.out.println("2. Remove User");
+        System.out.println("3. Display All User");
+        System.out.println("4. Register New Restaurant");
+        System.out.println("5. Remove Restaurant");
+        System.out.println("6. Display All Restaurant");
+        System.out.println("0. Exit");
+        
+        System.out.print("Enter choice: ");
+        
+        int choice = InputHelper.readInt(sc);
+        
+        switch(choice){
+            case 1 :
+                System.out.print("Enter IC number  : ");
+                String ic = InputHelper.readString(sc);
+                System.out.println();
+                
+                System.out.print("Enter name : ");
+                String name = InputHelper.readString(sc);
+                System.out.println();
+                
+                System.out.print("Enter phone number : ");
+                String phone = InputHelper.readString(sc);
+        }
        
     }
 
